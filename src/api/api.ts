b6a28fe79ca5daa1129c4f1b79bc8b25b1f0571f
Mohsanul786 <mdmohsan2407@gmail.com>
2025-06-29 03:21:56 +0530

    fix: backend url

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:4000/api/v1"
-    : "https://warpevents-backend.onrender.com/";
+    : "https://warpevents-backend.onrender.com/api/v1";
 
 export const api = axios.create({
   baseURL: BASE_URL,
